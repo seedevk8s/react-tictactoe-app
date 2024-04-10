c4dd909dc569dc06f191dc868472ca164790cdc6
seedevk8s <seedevk8s@gmail.com>
2024-04-10 01:37:52 +0000

    CRA 원내용 정리

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import "./App.css"
 
 function App() {
   return (
-    <div>
-
+    <div className="game">
+      <div className="game-board"> 
+        game-board
+      </div>
+      <div className="game-info">
+        game-info
+      </div>
     </div>
   );
 }
