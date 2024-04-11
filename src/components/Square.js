96e86dc34a90dd8f705d92f364d8a7f2956a298e
seedevk8s <seedevk8s@gmail.com>
2024-04-11 12:01:19 +0000

    무언가 기억하는 state 추가

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,10 +2,19 @@ import React from "react";
 import "./Square.css"
 
 export default class Square extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: null,
+        }
+    }
+
     render() {
         return (
-            <button className="square" onClick={() => {console.log('클릭')}}>
-                {this.props.value}
+            <button className="square" onClick={() => {/* console.log('클릭') */ this.setState({value: 'X'})}}>
+                {/* {this.props.value} */}
+                {this.state.value}
             </button>
         )
     }
